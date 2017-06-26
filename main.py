92e2d7594313476bc7e427a33e4e65ad9e712e4f
Mark Kovacs <kovacsmark89@gmail.com>
2017-06-26 17:55:26 +0200

    Planets pagination implemented with residents modal view

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 
-from flask import Flask, render_template, request, url_for, jsonify
+from flask import Flask, render_template, request, url_for, jsonify, redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 
@@ -7,9 +7,39 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index_page():
-    """Show index page."""
-    return render_template('index.html')
+def redirect_root():
+    """Redirection of root."""
+    return redirect(url_for('planets'))
+
+
+@app.route('/planets')
+def planets():
+    """Show a planets page."""
+    if request.args.get('page'):
+        page_number = request.args['page']
+        response = requests.get('http://swapi.co/api/planets/?page={}'.format(page_number)).json()
+    else:
+        page_number = '1'
+        response = requests.get('http://swapi.co/api/planets/').json()
+
+    if response.get('detail') == 'Not found':
+        return 'Page not found'
+
+    planets = response['results']
+
+    start = (int(page_number) - 1) * 10 + 1
+    end = start + len(planets) - 1
+    displayed_planets = '{} - {}'.format(start, end)
+
+    page = {
+        'current': page_number,
+        'next': response['next'],
+        'prev': response['previous'],
+        'displayed_planets': displayed_planets,
+        'total': response['count']
+        }
+
+    return render_template('index.html', planets=planets, page=page)
 
 # new1 = generate_password_hash('password1', method='pbkdf2:sha512:80000', salt_length=8)
 
--- static/script.js
+++ static/script.js
@@ -1,4 +1,123 @@
+var residents = new Array;
+
+$.ajax({
+    dataType: "json",
+    url: 'http://swapi.co/api/planets/',
+    success: function(response) {
+        return response['results'];
+    }
+});
+
+
+function directToPreviousPage () {
+    window.location.replace($('#prev').data('prev'));
+}
+
+function directToNextPage () {
+    window.location.replace($('#next').data('next'));
+}
+
+$('#prev').on('click', directToPreviousPage);
+$('#next').on('click', directToNextPage);
 
 
 
 
+function createModal () {
+
+    var clickedLink = $(event.target);
+    var linksOfResidents = clickedLink.data('residents');
+
+    linksOfResidents = linksOfResidents.replace(/\[(.*?)\]/g,"$1");
+    linksOfResidents = linksOfResidents.replace(/'/g, '');
+    linksOfResidents = linksOfResidents.split(', ');
+
+    var modal = $('#modal');
+    var modalContent = $('#modal-content');
+    var table = $('<table></table>');
+
+
+
+
+
+    localStorage.setItem('residents', JSON.stringify(new Array()));
+
+    for (let i = 0; i < linksOfResidents.length; i++) {
+        // $.ajax({
+        //     dataType: "json",
+        //     url: linksOfResidents[i],
+        //     success: function(response) {
+        //         residents.push({
+        //             name: response.name,
+        //             height: response.height,
+        //             mass: response.mass,
+        //             hair_color: response.hair_color,
+        //             skin_color: response.skin_color,
+        //             eye_color: response.eye_color,
+        //             birth_year: response.birth_year,
+        //             gender: response.gender
+        //         });
+        //     }
+        // });
+
+        $.get(linksOfResidents[i]).done(function(response) {
+
+            resident = {
+                name: response.name,
+                height: response.height,
+                mass: response.mass,
+                hair_color: response.hair_color,
+                skin_color: response.skin_color,
+                eye_color: response.eye_color,
+                birth_year: response.birth_year,
+                gender: response.gender
+            };
+            // var residents = JSON.parse(localStorage.getItem('residents'));
+            // residents.push(resident);
+            // localStorage.setItem('residents', JSON.stringify(residents))
+    
+
+
+            var name = $('<td>' + resident.name + '</td>');
+            var height = $('<td>' + resident.height + '</td>');
+            var mass = $('<td>' + resident.mass + '</td>');
+            var hairColor = $('<td>' + resident.hair_color + '</td>');
+            var skinColor = $('<td>' + resident.skin_color + '</td>');
+            var eyeColor = $('<td>' + resident.eye_color + '</td>');
+            var birthYear = $('<td>' + resident.birth_year + '</td>');
+            var gender = $('<td>' + resident.gender + '</td>');
+
+            var row = $('<tr></tr>');
+            row.append(name, height, mass, hairColor, skinColor, eyeColor, birthYear, gender);
+            table.append(row);
+            modalContent.append(table);
+
+        } );
+    }
+
+
+    // residents = JSON.parse(localStorage.getItem('residents'));
+    // alert('logged residents1');
+    // console.log(residents);
+    // alert('logged residents2');
+
+    // for (let i = 0; i < residents.length; i++) {
+    //     var name = $('<td>' + residents[i].name + '</td>');
+    //     var height = $('<td>' + residents[i].height + '</td>');
+    //     var mass = $('<td>' + residents[i].mass + '</td>');
+    //     var hairColor = $('<td>' + residents[i].hair_color + '</td>');
+    //     var skinColor = $('<td>' + residents[i].skin_color + '</td>');
+    //     var eyeColor = $('<td>' + residents[i].eye_color + '</td>');
+    //     var birthYear = $('<td>' + residents[i].birth_year + '</td>');
+    //     var gender = $('<td>' + residents[i].gender + '</td>');
+
+    //     var row = $('<tr></tr>');
+    //     row.append(name, height, mass, hairColor, skinColor, eyeColor, birthYear, gender);
+    //     table.append(row);
+    //     modalContent.append(table);
+    // }
+    modal.show();
+    modalContent.show();
+}
+
+$('.residents').on('click', createModal);
